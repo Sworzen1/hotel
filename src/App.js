010bb4dcd24dd06217e6338cfbaf723a04f78489
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-19 19:53:58 +0100

    Import and add Rooms component to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Nav from "./Nav"
 import Baner from "./Baner"
+import Rooms from "./Rooms"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Baner />
+      <Rooms />
     </div>
   );
 }
