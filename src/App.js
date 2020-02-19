85bd66bf7e99946e9dfdd86c8513d36e127a55e8
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-19 16:26:58 +0100

    import baner and nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Nav from "./Nav"
+import Baner from "./Baner"
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <Baner />
     </div>
   );
 }
