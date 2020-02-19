0b705563d55b9c88776c8e80feda71270a505ec7
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-19 20:42:42 +0100

    Import and add component Informations to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from "./Nav"
 import Baner from "./Baner"
 import Rooms from "./Rooms"
+import Information from "./Information"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Baner />
       <Rooms />
+      <Information />
     </div>
   );
 }
