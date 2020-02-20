f912fe38bfc2fc4395c7c32a433970edc77f2ff2
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-20 01:21:18 +0100

    Import Contact component to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Baner from "./Baner"
 import Rooms from "./Rooms"
 import Information from "./Information"
 import Restaurant from "./Restaurant"
+import Contact from "./Contact"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Rooms />
       <Information />
       <Restaurant />
+      <Contact />
     </div>
   );
 }
