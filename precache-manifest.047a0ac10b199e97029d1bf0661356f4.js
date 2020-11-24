052bb08ee49a9782c4c1d1ef058214fd2cf520f5
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-11-24 21:36:28 +0100

    Updates

---
+++ precache-manifest.047a0ac10b199e97029d1bf0661356f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504c60db438c2a441d6f9102a4a7b14e",
+    "url": "/hotel/index.html"
+  },
+  {
+    "revision": "9b2450855d92f781e990",
+    "url": "/hotel/static/css/main.a90d15d0.chunk.css"
+  },
+  {
+    "revision": "12f9adaecd2cc4eb6464",
+    "url": "/hotel/static/js/2.6474684c.chunk.js"
+  },
+  {
+    "revision": "cbe9d86c4ff38eb5938f6cad57bd7313",
+    "url": "/hotel/static/js/2.6474684c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2450855d92f781e990",
+    "url": "/hotel/static/js/main.37196d57.chunk.js"
+  },
+  {
+    "revision": "edef790caca7d32bdcb2",
+    "url": "/hotel/static/js/runtime-main.90b00c9c.js"
+  },
+  {
+    "revision": "d4ce27a8183a9e2d20020e3c9af2fe91",
+    "url": "/hotel/static/media/baner.d4ce27a8.jpg"
+  },
+  {
+    "revision": "114987f63c1702683284ffc209f2cfe1",
+    "url": "/hotel/static/media/diving.114987f6.jpg"
+  },
+  {
+    "revision": "267f5d4dd2cbc0a8a4856c892fbfbdfe",
+    "url": "/hotel/static/media/easy.267f5d4d.jpg"
+  },
+  {
+    "revision": "f4d57beca1d7324bf22d779e49567b6b",
+    "url": "/hotel/static/media/hard.f4d57bec.jpg"
+  },
+  {
+    "revision": "4775095e20419babd4b28cb2a222dffc",
+    "url": "/hotel/static/media/island.4775095e.jpg"
+  },
+  {
+    "revision": "0d691d05ff9ffdb8e1fe8fbc08a6a9ff",
+    "url": "/hotel/static/media/medium.0d691d05.jpg"
+  },
+  {
+    "revision": "eda3b69d03661ac48908189365eb20fa",
+    "url": "/hotel/static/media/reception.eda3b69d.jpg"
+  },
+  {
+    "revision": "8a0c40ea3c29bfe021d09c60a6366995",
+    "url": "/hotel/static/media/restaurant.8a0c40ea.jpg"
+  },
+  {
+    "revision": "bcde56e5e6843c74c20a30b1f2e1d933",
+    "url": "/hotel/static/media/spa.bcde56e5.jpg"
+  }
+]);
